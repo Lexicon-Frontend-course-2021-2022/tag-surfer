2ffa23ad2cefa2f8d06ab3970b8ff78a8223329c
Thomas Hellström <rel@xed.se>
2021-10-11 16:48:46 +0200

    Basic tag handling (add/remove/reset)

--- src/app.js
+++ src/app.js
@@ -1,19 +1,50 @@
+const tags = document.querySelector("#tags");
 
-// Just some testing for now
+// Reset app (Remove all tags)
+const resetTags = () => {
+  let tag;
+  while (tag = document.querySelector('.tag')) {
+    tags.removeChild(tag);
+  }
+}
 
-// Get recent photos, 50 per page, first page
-// flickrPhotosGetRecent(50).then(result => console.log(result));
-
-// Get recent photos, 12 per page, 10:th page
-//flickrPhotosGetRecent(12, 10).then(result => console.log(result));
-
-
-const popups = {
-  welcome: document.querySelector('#welcome')
+// Remove one tag. If last tag, also remove reset
+const removeTag = e => {
+  const me = e.target;
+  tags.removeChild(me);
+  if (tags.children.length == 1) {
+    tags.removeChild(document.querySelector("#reset"));
+  }
 };
 
-popups.welcome.addEventListener('click', () => {
-  document.body.removeEventListener('click', welcome);
-  document.body.removeChild(welcome);
-});
 
+// Create new tag item and add to list. If this is first tag, add reset.
+const addTag = (text, value) => {
+
+  // Add "Reset" before first tag
+  if (!tags.children.length) {
+    const reset = document.createElement('div');
+    reset.classList.add(['tag']);
+    reset.classList.add(['reset']);
+    reset.id = 'reset';
+    reset.innerText = "Reset";
+    reset.addEventListener('click', resetTags);
+    tags.appendChild(reset);
+  }
+
+  // Add tag
+  const div = document.createElement('div');
+  div.classList.add(['tag']);
+  div.classList.add(['tag-item']);
+  div.innerText = value;
+  div.dataset.tag = value;
+  div.addEventListener('click', removeTag);
+  tags.appendChild(div);
+};
+
+
+addTag("blue", "Blue");
+addTag("yellow", "Yellow");
+addTag("green", "Green");
+addTag("white", "White");
+addTag("black", "Black");
