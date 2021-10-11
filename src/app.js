c97c7e9897bd24324926ec14f932f228afe79186
Thomas Hellström <rel@xed.se>
2021-10-11 14:42:42 +0200

    Experimenting with responsive design

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@
 // Just some testing for now
 
 // Get recent photos, 50 per page, first page
-flickrPhotosGetRecent(50).then(result => console.log(result));
+// flickrPhotosGetRecent(50).then(result => console.log(result));
 
 // Get recent photos, 12 per page, 10:th page
-flickrPhotosGetRecent(12, 10).then(result => console.log(result));
+//flickrPhotosGetRecent(12, 10).then(result => console.log(result));
+
+
+const popups = {
+  welcome: document.querySelector('#welcome')
+};
+
+popups.welcome.addEventListener('click', () => {
+  document.body.removeEventListener('click', welcome);
+  document.body.removeChild(welcome);
+});
+
