81f771ffbe69f23cddc5b11c03c7b34ef667a411
Thomas Hellström <rel@xed.se>
2021-10-14 15:43:33 +0200

    Cleaning up view-details.js

--- src/view-details.js
+++ src/view-details.js
@@ -5,33 +5,24 @@
  * ========================================================================== */
 
 
-
-/*
- * 'click' event handler.
- *
- * Close details view and show thumbs instead
- */
-views.details.e.addEventListener('click', () => {
-  views.thumbs.show();
-});
-
-/*
- * onHide() callback (see views.js) to remove disabled tags from tag bar
- */
-views.details.onHide = () => {
-  tags.removeDisabled();
-};
-
-/*
- * onShow() callback to add disabled tags and set image
- */
+/* ============================================================================
+ * onShow() override to add disabled tags to tag-bar and set image
+ * ========================================================================== */
 views.details.onShow = indata => {
 
   // Add disabled tags to tag bar
-  indata.tags.forEach(tag => {
-    tags.add(tag, false);
-  });
+  indata.tags.forEach(tag => tags.add(tag, false));
 
   // Set background image & show details
   views.details.e.style.backgroundImage = `url('${indata.url}')`;
-}
+};
+
+/* ============================================================================
+ * onHide() override (see views.js) to remove disabled tags from tag bar
+ * ========================================================================== */
+views.details.onHide = () => tags.removeDisabled();
+
+/* ============================================================================
+ * 'click' event handler. Close details and show thumbs instead.
+ * ========================================================================== */
+views.details.e.addEventListener('click', () => views.thumbs.show());
